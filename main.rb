5159a5b705d41a0e311ae0db49961c86250b2f60
Lukas Dolezal <ldolezal@zendesk.com>
2021-02-19 21:13:24 +0100

    Main args and examples in files

--- main.rb
+++ main.rb
@@ -1,37 +1,26 @@
 require './lib/shapes_solver'
 require './shapes/pentomino'
 
-DEBUG = true
+if (ARGV[0] == "--debug")
+  DEBUG = true
+  ARGV.slice!(0, 1)
+end
 
-# solver = ShapesSolver.new(
-#   Pentomino::ALL_12,
-#   Shape.from_string(%{
-#     O   O
-#     OO OO
-#     OOOOO
-#     O O O
-#     OOOOO
-#      OOO
-#       O
-#      OOO
-#     OOOOO
-#     OOOOO
-#     OOOOOO   O
-#     OOOOOOOOOO
-#      OOOOOOO
-#   })
-# )
+if ARGV.empty?
+  puts "ruby main.rb [--debug] problem_file.txt"
+  exit 1
+end
 
-solver = ShapesSolver.new(
-  [Pentomino::Y, Pentomino::L, Pentomino::U, Pentomino::V, Pentomino::I],
-  Shape.from_string(%{
-    KKKKK
-    KKKKK
-    KKKKK
-    KKKKK
-    KKKKK
-  })
-)
+def solver_from_file(filename)
+  content_lines = File.readlines(filename)
+  shapes = content_lines[0].strip.split(",").flat_map { |shape_name| Pentomino.const_get(shape_name) }
+
+  target = Shape.from_string(content_lines.drop(2).join)
+
+  ShapesSolver.new(shapes, target)
+end
+
+solver = solver_from_file(ARGV.last)
 
 puts "Shapes:"
 puts solver.shapes.join("\n\n")
